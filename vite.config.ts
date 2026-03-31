7bd228e2a083d694094c847035d4a90c23cb2c85
Daniil-developer18 <danya.krasnikov02@mail.ru>
2026-03-31 20:34:26 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/replica-of-painting2/'
+  base: '/replica-of-painting2/',
   plugins: [react()],
 })
